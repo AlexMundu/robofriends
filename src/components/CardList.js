6797cbe135d11cb07e79796893f9af372f48b87b
Alexis Muñoz <alexismun53@gmail.com>
2018-08-11 19:21:42 -0500

    Initial commit

---
+++ src/components/CardList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './Card';
+
+
+const CardList = ({ robots }) => {
+	return (
+		<div>
+			{ 
+				robots.map((robot, i) => {
+					return <Card 
+					key={ i } 
+					name={ robot.name } 
+					id={ robot.id } 
+					email={ robot.email }/>
+				})
+			}
+		</div>
+	)
+}
+
+export default CardList;
